9b8cc0d3ee3c53c300cbcd17cca2405f83d5e967
Abdellah Azeroual <abazerou@e3r8p11.1337.ma>
2024-08-26 17:11:09 +0100

    added front to dashboard

--- src/app/Dashboard/page.jsx
+++ src/app/Dashboard/page.jsx
@@ -9,9 +9,8 @@ const montserrat = Montserrat({
 
 function Dashboard() {
   return (
-    <nav className="bg-[#F4F4FF] py-4 h-[90px] flex items-center">
+    <nav className={`bg-[#F4F4FF] py-4 h-[90px] flex items-center ${montserrat.className}`}>
       <div className="container mx-auto flex justify-between">
-        <div className="text-blue font-bold text-xl">LOGO</div> 
         <div className="relative">
           <input
             type="text"
@@ -38,7 +37,7 @@ function Dashboard() {
             type="button"
             data-dropdown-toggle="userDropdown"
             data-dropdown-placement="bottom-start"
-            class="w-10 h-10 rounded-full cursor-pointer"
+            className="w-10 h-10 rounded-full cursor-pointer"
             src="/images/avatar.svg"
             alt="User dropdown"
             width="100"
@@ -46,27 +45,27 @@ function Dashboard() {
           />
           <div
             id="userDropdown"
-            class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 "
+            className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 "
             
           >
-            <div class="px-4 py-3 text-sm text-gray-900 dark:text-white"></div>
+            <div className="px-4 py-3 text-sm text-gray-900 dark:text-white"></div>
             <ul
-              class="py-2 text-sm text-gray-700 dark:text-gray-200"
+              className="py-2 text-sm text-gray-700 dark:text-gray-200"
               aria-labelledby="avatarButton"
             >
               <li>
                 <a
                   href="#"
-                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                 >
                   Settings
                 </a>
               </li>
             </ul>
-            <div class="py-1">
+            <div className="py-1">
               <a
                 href="#"
-                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
               >
                 Sign out
               </a>
