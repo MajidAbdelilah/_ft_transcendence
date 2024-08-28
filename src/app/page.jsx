5d6229e7cdc6af299739063a49adcb75332ae756
Abdellah Azeroual <abazerou@e3r9p9.1337.ma>
2024-08-28 19:03:21 +0100

    responsive for phones

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -24,22 +24,22 @@ export default function App() {
   return (
     <body>
       <div className={` h-[100vh]${montserrat.className}`}>
-        <nav className="flex justify-between pl-20 pt-18 pr-20 w-full items-center h-[200px] fixed">
-          <Image src="images/logo.svg" alt="Logo" width="150" height="150" />
-          <div className="flex gap-2 ">
-            <button onClick={handleSignUp} type="button" className="text-white bg-[#111B47] hover:bg-[#0e1739] hover:ring-4 focus:ring-[#1d2f7a] font-bold rounded-full text-lg h-[70px] w-[180px] text-center me-2 mb-2 transition duration-300 ease-in-out shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] border-solid border-b-4 border-gray-600">Sign up</button>
-            <button onClick={handleLogin} type="button" className="text-white bg-[#111B47] hover:bg-[#0e1739] hover:ring-4 focus:ring-[#1d2f7a] font-bold rounded-full text-lg h-[70px] w-[180px] text-center me-2 mb-2 transition duration-300 ease-in-out shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] border-solid border-b-4 border-gray-600">Login</button>
+        <nav className="flex justify-between sm:pl-20 sm:pt-18 sm:pr-20 w-full sm:items-center sm:h-[200px] fixed">
+          <Image src="images/logo.svg" alt="Logo" width={150} height={150} className="sm:h-[150px] sm:w-[150px] w-[80px] h-[80px]" />
+          <div className="flex gap-2 pt-3">
+            <button onClick={handleSignUp} type="button" className="text-white bg-[#111B47] hover:bg-[#0e1739] hover:ring-4 focus:ring-[#1d2f7a] font-bold rounded-full text-md h-[60px] w-[130px] sm:text-lg sm:h-[70px] sm:w-[180px] text-center me-2 mb-2 transition duration-300 ease-in-out shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] border-solid border-b-4 border-gray-600">Sign up</button>
+            <button onClick={handleLogin} type="button" className="text-white bg-[#111B47] hover:bg-[#0e1739] hover:ring-4 focus:ring-[#1d2f7a] font-bold rounded-full text-md h-[60px] w-[130px] sm:text-lg sm:h-[70px] sm:w-[180px] text-center me-2 mb-2 transition duration-300 ease-in-out shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px] border-solid border-b-4 border-gray-600">Login</button>
           </div>
         </nav>
-        <div className="w-full flex justify-center items-center">
-        <div className="flex justify-around w-full items-center max-w-[2000px] h-[100vh]">
-          <div className="w-[50%] max-w-[800px]">
-            <h1 className={`font-bold xxl:text-[100px] text-[60px] flex flex-col  pb-5 ${montserrat.className}`}>
+        <div className="w-full flex justify-center items-center ">
+        <div className="sm:flex sm:justify-around sm:w-full items-center max-w-[2000px] h-[100vh] pt-[140px]">
+          <div className="w-[50%] sm:max-w-[800px] w-5/6 pl-8">
+            <h1 className={`font-bold xxl:text-[100px] sm:text-[60px] text-[30px] flex flex-col  pb-5 ${montserrat.className}`}>
               <span className="block -mb-[0.4em] font-extrabold text-[#091133]">ONLINE</span>
               <span className="block -mb-[0.4em] font-extrabold text-[#091133]">PING PONG</span>
               <span className="block font-extrabold text-[#091133]">GAME</span>
             </h1>
-            <p className={`text-lg  pt-2 text-[#505F98] ${montserrat.className}`}>
+            <p className={`text-md  pt-2 text-[#505F98] text-wrap ${montserrat.className}`}>
               Welcome to Ultimate Pong Arena, where the classic game meets modern competition.
               <br/>
               Dive into fast-paced matches, climb the leaderboards, and join a community of enthusiasts.
@@ -47,13 +47,13 @@ export default function App() {
               Ready for action? <span className="font-bold">Let the games begin!</span>
             </p>
           </div>
-          <div className="w-[50%] max-w-[600px]">
+          <div className="sm:w-[50%] sm:max-w-[600px] ">
             <Image
               src="images/pong.svg"
               alt="Pong"
               width={600}
               height={400}
-              className="pongImg w-full"
+              className="pongImg sm:w-full"
             />
           </div>
         </div>
