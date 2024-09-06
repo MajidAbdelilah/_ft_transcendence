013df015e9122e4521474243170a8defe8ddbd60
Hassan Ed-Dyb <hed-dyb@e3r3p5.1337.ma>
2024-09-06 15:45:12 +0100

    saving

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -4,14 +4,14 @@
 
 import Sidebar_test from "./components/sidebar";// import the real one as a componant later
 import Navbar_test from "./components/navbar";// import the real one as a componant later
-
+import Image from 'next/image';
 
 
 
 
 import './style.css';
 
-// // import Image from 'next/image';
+
 // // import Link from 'next/link';
 
 // import { Sidebar } from '../Dashboard/sidebar';
@@ -27,15 +27,17 @@ import './style.css';
 
 
 
-// // or w-full h-full  flex gap-4 
+{/* <div className="w-full pb-40 max-w-[100%] mx-auto relative">
+<hr className="border-[#242F5C] border-t-1" />
+<Image src="/images/avatarprofile.svg" alt="avatarprofile" width={50} height={50} className="mx-auto pt-9 left-5 absolute left-[-7px]" />
 
+<p className="text-center text-lg font-normal text-[#242F5C] pt-10 absolute left-12 ">John Doe</p>
 
-// h-full
-// h-screen: 100% of the viewport height.
-// h-1/2: 50% of the parent height.
-// h-auto: Height adjusts based on content.
-// h-[size]: Custom height (e.g., h-64 for 16rem).
+<p className="text-center text-[12px] font-light text-[#8988DE] pt-10 absolute left-12 top-6 ">My Account</p>
+<Image src="/images/logout.svg" alt="arrow" width={20} height={20} className="mx-auto pt-10 absolute right-[12px] top-2 cursor-pointer" />
+</div> */}
 
+{/* <div class="absolute left-0 top-0 h-16 w-16 ...">01</div> */}
 
 
 export default function ChatPage() {
@@ -51,13 +53,18 @@ export default function ChatPage() {
                     <div className="Boxes flex h-full w-full border-2 border-[#C6C6E1] bg-[#F4F4FF] rounded-xl flex-row-revers">
 
                         
-                        {/* <div className="Boxe1 h-full rounded-tl-xl rounded-bl-xl p-10 flex-1 bg-[#555555] ">
-                            <h2>hellow world</h2>
+                        <div className="Boxe1 h-full w-1/3 rounded-tl-xl rounded-bl-xl flex-1 p-4 border-r-2 border-[#C6C6E1] bg-[#F4F4FF] ">
+                            {/* <div>hfugfrf<div/> */}
+                            <Image src="/images/avatarprofile.svg" alt="avatarprofile" width={60} height={60} className="left-0 top-0"/>
+                            <p classname="text-">John Doe</p>
                         </div> 
 
-                        <div className="Boxe2 h-full rounded-tr-xl rounded-br-xl flex-2 p-10 bg-[#f4ffff]">
+
+
+
+                        <div className="Boxe2 h-full w-2/3 rounded-tr-xl rounded-br-xl flex-2 p-4 bg-[#F4F4FF ]">
                             <h2>hellow world</h2>
-                        </div>  */}
+                        </div> 
                     </div>
 
 
