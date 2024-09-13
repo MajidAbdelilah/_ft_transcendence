64e545bb614a59e3aaaf7424b367ad39bed953d9
Hassan Ed-Dyb <hed-dyb@e3r8p2.1337.ma>
2024-09-13 18:09:27 +0100

    saving..

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -36,13 +36,13 @@ export default function ChatPage() {
           {/* for the padding to make chlids far from the bar and the sidebar */}
           <div className="boxes flex h-full w-full border-2 border-[#C6C6E1] bg-[#F4F4FF] rounded-xl flex-row-revers">
             {/* friendsBox ------------------------------------------------------- */}
-            <div className="friendsBox h-full w-1/3 rounded-tl-xl rounded-bl-xl flex-1 p-4 border-r-2 border-[#C6C6E1] bg-[#F4F4FF] ">
+            <div className="friendsBox h-full w-1/4 rounded-tl-xl rounded-bl-xl flex-1 p-4 border-r-2 border-[#C6C6E1] bg-[#F4F4FF] ">
             
-              <div className="menuList p-5">
+              <div className="menuList p-5 hidden lg:block">
 
 
 
-                <div className="profileInfo w-full flex items-center overflow-hidden">
+                <div className="profileInfo w-full flex items-center  overflow-hidden">
                   <Image
                     src="/images/avatarprofile.svg"
                     alt="avatarprofile"
@@ -50,18 +50,44 @@ export default function ChatPage() {
                     height={75}
                     className="left-0 top-0 "
                   />
-                  <div className=" ml-4 flex-col flex-grow hidden lg:block">
-                      <h3 className="text-3xl  top-0 right-0 text-[#242F5C] left">John Doe</h3>
+                  <div className=" ml-4  ">
+                      <h3 className="text-3xl  top-0 left-0 text-[#242F5C] ">John Doe</h3>
                       <p className="text-sm text-[#302FA5] left">Online</p>
                   </div>
                 </div>
 
 
+                <h2 className="text-center text-2xl my-8 py-2 rounded-full bg-[#9191D6] bg-opacity-20 text-[#242F5C] ">Conversations</h2>
 
-                <h2>Messages List</h2>
-                {/* div_message_list */}
-              
-              
+
+
+                <div className="MessagesList bg-red-50 w-full flex flex-col items-center overflow-hidden">
+                  
+                  <div className="friendInfo my-2 w-full flex flex-rowitems-center overflow-hidden">
+                    <Image
+                      src="/images/avatarprofile.svg"
+                      alt="avatarprofile"
+                      width={50}
+                      height={50}
+                      className="left-0 top-0 "
+                    />
+                    <div className=" ml-2">
+                      <h3 className="text-2xl top-0 left-0 text-[#242F5C]">John Wick</h3>
+                      <p className="text-xs text-[#302FA5] overflow-hidden">Graet Game! Rematch tomorrow?</p>
+                    </div>
+
+
+                    
+                    <span className="text-xs ml-auto">2:15 AM</span>
+                   
+
+                  </div>
+
+
+
+
+ 
+                  </div>
               
               
               
@@ -79,7 +105,7 @@ export default function ChatPage() {
               
             </div>
             {/* messagesBox ------------------------------------------ */}
-            <div className="messagesBox h-full w-2/3 rounded-tr-xl rounded-br-xl flex-2 p-4 bg-[#F4F4FF ]">
+            <div className="messagesBox h-full w-3/4 rounded-tr-xl rounded-br-xl flex-2 p-4 bg-[#F4F4FF ]">
               <h2>hellow world</h2>
             </div>
           </div>
