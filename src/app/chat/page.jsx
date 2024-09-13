13da890cbe72793fb12c2d066b0642ff8750e232
Hassan Ed-Dyb <hed-dyb@e3r8p2.1337.ma>
2024-09-13 15:36:47 +0100

    saving

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -12,14 +12,12 @@ const montserrat = Montserrat({
 
 
 
-{/* <div className="profil-infos">
-  <div className="image">
-    <img src="/images/avatarprofile.svg" />
-  </div>
-  <div className="infos">
-    <h3>Knetero Jack</h3>
-    <span className="status">Online</span>
-  </div>
+{/* <div className="w-full pb-40 max-w-[100%] mx-auto relative">
+          <hr className="border-[#242F5C] border-t-1" />
+          <Image src="/images/avatarprofile.svg" alt="avatarprofile" width={50} height={50} className="mx-auto pt-9 left-5 absolute left-[-7px]" />
+          <p className="text-center text-lg font-normal text-[#242F5C] pt-10 absolute left-12 ">John Doe</p>
+          <p className="text-center text-[12px] font-light text-[#8988DE] pt-10 absolute left-12 top-6 ">My Account</p>
+          <Image src="/images/logout.svg" alt="arrow" width={20} height={20} className="mx-auto pt-10 absolute right-[12px] top-2 cursor-pointer" />
 </div> */}
 
 
@@ -40,23 +38,26 @@ export default function ChatPage() {
             {/* friendsBox ------------------------------------------------------- */}
             <div className="friendsBox h-full w-1/3 rounded-tl-xl rounded-bl-xl flex-1 p-4 border-r-2 border-[#C6C6E1] bg-[#F4F4FF] ">
             
+              <div className="menuList p-5">
 
 
 
-              <div className="menuList">
-                <div className="profileInfo w-full flex  items-center overflow-hidden">
+                <div className="profileInfo w-full flex items-center overflow-hidden">
                   <Image
                     src="/images/avatarprofile.svg"
                     alt="avatarprofile"
-                    width={70}
-                    height={70}
+                    width={75}
+                    height={75}
                     className="left-0 top-0 "
                   />
-                  <div className=" ml-4 flex-col flex-grow">
-                      <p className="text-xl top-0 right-0 text-[#242F5C] left">John Doe</p>
+                  <div className=" ml-4 flex-col flex-grow hidden lg:block">
+                      <h3 className="text-3xl  top-0 right-0 text-[#242F5C] left">John Doe</h3>
                       <p className="text-sm text-[#302FA5] left">Online</p>
                   </div>
                 </div>
+
+
+
                 <h2>Messages List</h2>
                 {/* div_message_list */}
               
