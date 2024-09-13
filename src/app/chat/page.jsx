c4998e5aa981e8deb2bf303287f047a5ffcba134
Hassan Ed-Dyb <hed-dyb@e3r8p2.1337.ma>
2024-09-13 14:25:13 +0100

    save

--- src/app/chat/page.jsx
+++ src/app/chat/page.jsx
@@ -2,6 +2,7 @@ import Sidebar_test from "./components/sidebar"; // import the real one as a com
 import Navbar_test from "./components/navbar"; // import the real one as a componant later
 import Image from "next/image";
 
+
 import "./style.css";
 import { Inter, Montserrat } from "next/font/google";
 const montserrat = Montserrat({
@@ -9,64 +10,75 @@ const montserrat = Montserrat({
   variable: "--font-montserrat",
 });
 
-// // import Link from 'next/link';
 
-// import { Sidebar } from '../Dashboard/sidebar';
 
-// import { Inter, Montserrat } from 'next/font/google'
-// const montserrat = Montserrat({
-//     subsets: ['latin'],
-//     variable: '--font-montserrat',
-//   })
+{/* <div className="profil-infos">
+  <div className="image">
+    <img src="/images/avatarprofile.svg" />
+  </div>
+  <div className="infos">
+    <h3>Knetero Jack</h3>
+    <span className="status">Online</span>
+  </div>
+</div> */}
 
-{
-  /* <div className="w-full pb-40 max-w-[100%] mx-auto relative">
-<hr className="border-[#242F5C] border-t-1" />
-<Image src="/images/avatarprofile.svg" alt="avatarprofile" width={50} height={50} className="mx-auto pt-9 left-5 absolute left-[-7px]" />
 
-<p className="text-center text-lg font-normal text-[#242F5C] pt-10 absolute left-12 ">John Doe</p>
 
-<p className="text-center text-[12px] font-light text-[#8988DE] pt-10 absolute left-12 top-6 ">My Account</p>
-
-<Image src="/images/logout.svg" alt="arrow" width={20} height={20} className="mx-auto pt-10 absolute right-[12px] top-2 cursor-pointer" />
-</div> */
-}
-
-{
-  /* <div class="absolute left-0 top-0 h-16 w-16 ...">01</div> */
-}
 
 export default function ChatPage() {
   return (
     <div className={`flex flex-col h-screen ${montserrat.className}`}>
       <Navbar_test />
-      <div className="flex flex-1">
+      <div className="parent flex flex-1">
+        <div className="sidebar hidden md:block">
         <Sidebar_test />
-        <div className="flex-1 p-10">
+        </div>
+        
+        <div className="chattSection flex-1 p-10">
           {/* for the padding to make chlids far from the bar and the sidebar */}
-          <div className="Boxes flex h-full w-full border-2 border-[#C6C6E1] bg-[#F4F4FF] rounded-xl flex-row-revers">
-            {/* Boxe1 ------------------------------------------------------- */}
-            <div className="Boxe1 h-full w-1/3 rounded-tl-xl rounded-bl-xl flex-1 p-4 border-r-2 border-[#C6C6E1] bg-[#F4F4FF] ">
-              {/* profile ------------------------------ */}
-              <div className="w-full flex items-center overflow-hidden">
-                <Image
-                  src="/images/avatarprofile.svg"
-                  alt="avatarprofile"
-                  width={60}
-                  height={60}
-                  className="left-0 top-0"
-                />
-                <div className="ml-4 flex-col">
-                    <p className="text-xl top-0 right-0 text-[#242F5C] left">John Doe</p>
-                    <p className="text-sm text-[#302FA5] left">Online</p>
+          <div className="boxes flex h-full w-full border-2 border-[#C6C6E1] bg-[#F4F4FF] rounded-xl flex-row-revers">
+            {/* friendsBox ------------------------------------------------------- */}
+            <div className="friendsBox h-full w-1/3 rounded-tl-xl rounded-bl-xl flex-1 p-4 border-r-2 border-[#C6C6E1] bg-[#F4F4FF] ">
+            
+
+
+
+              <div className="menuList">
+                <div className="profileInfo w-full flex  items-center overflow-hidden">
+                  <Image
+                    src="/images/avatarprofile.svg"
+                    alt="avatarprofile"
+                    width={70}
+                    height={70}
+                    className="left-0 top-0 "
+                  />
+                  <div className=" ml-4 flex-col flex-grow">
+                      <p className="text-xl top-0 right-0 text-[#242F5C] left">John Doe</p>
+                      <p className="text-sm text-[#302FA5] left">Online</p>
+                  </div>
                 </div>
-                
+                <h2>Messages List</h2>
+                {/* div_message_list */}
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
               </div>
-              {/* list of conversation ----------------------------------- */}
-              <div></div>
+
+
+
+              
+              
+              
             </div>
-            {/* Boxe2 ------------------------------------------ */}
-            <div className="Boxe2 h-full w-2/3 rounded-tr-xl rounded-br-xl flex-2 p-4 bg-[#F4F4FF ]">
+            {/* messagesBox ------------------------------------------ */}
+            <div className="messagesBox h-full w-2/3 rounded-tr-xl rounded-br-xl flex-2 p-4 bg-[#F4F4FF ]">
               <h2>hellow world</h2>
             </div>
           </div>
