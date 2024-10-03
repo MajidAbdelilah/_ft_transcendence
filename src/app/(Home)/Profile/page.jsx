9e62588dc84c71c562cf0c359d17e4d3e675d989
Hassan Ed-Dyb <hed-dyb@e3r3p5.1337.ma>
2024-10-03 16:25:39 +0100

    save

--- src/app/(Home)/Profile/page.jsx
+++ src/app/(Home)/Profile/page.jsx
@@ -2,20 +2,33 @@
 
 import MatchHistory from "../Dashboard/MatchHistory";
 import DashProvider from "../Dashboard/Dashcontext";
-// export const DashContext = createContext();
-// import MatchHistory from "../Dashboard/MatchHistory";
+
+
+
+import { Inter, Montserrat } from "next/font/google";
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
+
+
+
+
+
+
+import LeaderBoard from "./components/LeaderBoard";
+
 
 export default function Profile() {
   return (
-    <div className="flex-1 overflow-y-auto p-4 flex flex-wrap items-center justify-center h-full">
-      <div className="p-8  bg-red-400 ">degwqrgrhthtwerhthththre</div>
-      <div className="p-8  bg-yellow-400 ">degwqrgrhthtwerhthththre</div>
-      {/* <PlayNow />
-        <Achievements />  */}
-      {/* <MatchHistory /> */}
-      <DashProvider>
-        <MatchHistory />
-      </DashProvider>
-    </div>
+    <DashProvider>
+      <div className={`flex-1 overflow-y-auto p-4 flex flex-wrap items-center justify-center h-full ${montserrat.variable}`}>
+
+      <LeaderBoard />
+      {/* <UserProfile /> */}
+      <MatchHistory />
+      </div>
+    </DashProvider>
   );
 }
