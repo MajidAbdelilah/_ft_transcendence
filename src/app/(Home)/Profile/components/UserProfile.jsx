bd0cadfe889fdab2d8c62bf7dd3053fda3ba3533
Hassan Ed-Dyb <hed-dyb@e3r3p5.1337.ma>
2024-10-04 11:03:22 +0100

    save

---
+++ src/app/(Home)/Profile/components/UserProfile.jsx
@@ -0,0 +1,77 @@
+// import { div } from "framer-motion/client";
+// import { DashContext } from "../../Dashboard/Dashcontext";
+// import { useContext } from "react";
+
+import { BsChatLeftText } from "react-icons/bs";
+import { MdOutlinePersonAddAlt } from "react-icons/md";
+import { LuUserX } from "react-icons/lu";
+// import { DashContext } from "../../Dashboard/Dashcontext";
+// import { useContext } from "react";
+// -- colors -----------------------------------------------------co
+//  #F4F4FF   #242F5C   #8988DE   #BCBCC9   #F4F4FF   #EAEAFF   #C0C7E0
+
+
+
+
+
+
+
+
+
+export default function UserProfile() {
+   
+  return (
+    <div className="flex shadow-md shadow-[#BCBCC9] border border-[#BCBCC9] rounded-2xl bg-[#F4F4FF] h-40 w-[80%]">
+      
+      <div className="part1 relative w-1/3 p-2 rounded-l-2xl bg-[#F4F4FF] border-[#BCBCC9] border-r-2 min-w-32 ">
+
+
+        <div className="flex flex-col items-center">
+            <img
+            src="/images/avatar3.svg"
+            alt="Profile"
+            className="absolute w-20 h-20 rounded-full border-2 border-[#BCBCC9] -top-10  shadow-md shadow-[#BCBCC9]"
+            />
+          <div className="mt-12 text-sm font-bold text-[#242F5C]">
+            John Smith
+          </div>
+          <span className="text-xs mt-1 text-[#8988DE]">Online</span>
+
+          <div className="flex flex-row mt-2 text-[#242F5C] ">
+            <BsChatLeftText className="mr-1 text-lg" />
+            <MdOutlinePersonAddAlt className="ml-1 text-xl" />
+          </div>
+        </div>
+      </div>
+
+      <div className="part2 w-2/3 p-4 flex flex-col items-end ml-auto   ">
+
+        <LuUserX className="blockIcon text-[#242F5C] text-3xl" />
+
+        <div className="level flex flex-col items-start w-full mb-4">
+          <span className="text-xs text-[#242F5C] font-semibold">Level 1</span>
+
+          
+          <div className="relative w-full h-3 bg-gray-300 rounded-full">
+            
+            <div
+              className="absolute top-0 left-0 h-3 bg-[#8988DE] rounded-full"
+              style={{ width: "60%" }}
+            ></div>
+          </div>
+
+          
+          <div className="flex justify-between w-full mt-1 text-[#242F5C] text-xs ">
+            <span >Next level</span>
+            <span >Level 2</span>
+          </div>
+
+
+        </div>
+        <button className="gameStats bg-[#242F5C] p-1  px-2 text-[#F4F4FF] text-xs rounded-full font-semibold">
+            GAME STATS
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/app/(Home)/Profile/page.jsx
+++ src/app/(Home)/Profile/page.jsx
@@ -17,16 +17,16 @@ const montserrat = Montserrat({
 
 
 
-import LeaderBoard from "./components/LeaderBoard";
+import UserProfile from "./components/UserProfile";
 
 
 export default function Profile() {
   return (
     <DashProvider>
       <div className={`flex-1 overflow-y-auto p-4 flex flex-wrap items-center justify-center h-full ${montserrat.variable}`}>
-
-      <LeaderBoard />
-      {/* <UserProfile /> */}
+      <UserProfile />
+      {/* <LeaderBoard /> */}
+      
       <MatchHistory />
       </div>
     </DashProvider>
