5b935297f358e9ace8a96d8cd7a5356a404be687
Hassan Ed-Dyb <hed-dyb@e3r3p5.1337.ma>
2024-10-04 14:12:35 +0100

    save

---
+++ src/app/(Home)/Profile/components/LeaderBoard.jsx
@@ -0,0 +1,18 @@
+
+
+
+
+export default function LeaderBoard({first, second, third}) {
+    return (
+        <div className="flex flex-col shadow-md shadow-[#BCBCC9] border border-[#BCBCC9] rounded-2xl bg-[#F4F4FF] h-40 w-[80%]">
+
+
+                <h1 className="text-[#444E74]  h-[18%] font-black text-center pt-5 tracking-wider  text-md md:text-lg lg:text-xl ">
+                    LEADER BOARD
+                </h1>
+                <div>dfshhdg</div>
+                <div>dfshhdg</div>
+
+        </div>
+    );
+}
--- src/app/(Home)/Profile/page.jsx
+++ src/app/(Home)/Profile/page.jsx
@@ -3,7 +3,8 @@
 import MatchHistory from "../Dashboard/MatchHistory";
 import DashProvider from "../Dashboard/Dashcontext";
 
-
+import UserProfile from "./components/UserProfile";
+import LeaderBoard from "./components/LeaderBoard";
 
 import { Inter, Montserrat } from "next/font/google";
 const montserrat = Montserrat({
@@ -14,18 +15,45 @@ const montserrat = Montserrat({
 
 
 
+//data ------------------------------------------
+let user1 = {
+  name: "John Wick",
+  avatar: "/images/avatarprofile.svg",
+  status: "Online",
+  level : 1,
+  score: "5-4",
+  result: "Win",
+  map: "Blue",
+};
 
 
+let user2 ={
+  avatar: "/images/avatar1.svg",
+  name: "Ali",
+  score: "5-4",
+  result: "Win",
+  map: "Blue",
+  status: "Offline",
+  level : 2,
+};
 
-import UserProfile from "./components/UserProfile";
-
+let user3 ={
+  avatar: "/images/avatar3.svg",
+  name: "Malcom Smith",
+  score: "3-4",
+  result: "Lose",
+  map: "Blue",
+  status: "Offline",
+  level : 3,
+};
 
 export default function Profile() {
   return (
     <DashProvider>
       <div className={`flex-1 overflow-y-auto p-4 flex flex-wrap items-center justify-center h-full ${montserrat.variable}`}>
-      <UserProfile />
-      {/* <LeaderBoard /> */}
+      
+      <UserProfile user={user1}/>
+      <LeaderBoard first={user1} second={user2} third={user3}/>
       
       <MatchHistory />
       </div>
