6af6fa4de107532a27312e985203d6483c6fc2d2
Hassan Ed-Dyb <hed-dyb@e3r3p5.1337.ma>
2024-10-04 17:19:44 +0100

    save

--- src/app/(Home)/Profile/components/LeaderBoard.jsx
+++ src/app/(Home)/Profile/components/LeaderBoard.jsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
 
- function BoardTemplate ({ user}) {
+ function BoardTemplate ({ number, user}) {
   return (
     <div className="flex items-center bg-[#E1E0F2] m-3 py-1 pl-3 rounded-full relative text-[#242F5C] lg:text-xl">
-    <h2 className="font-bold ml-1 ">01</h2>
+    <h2 className="font-bold ml-1 ">{number}</h2>
     <Image
       src={user.avatar}
       alt="avatarprofile"
@@ -23,9 +23,9 @@ export default function LeaderBoard({ first, second, third }) {
       <h1 className="text-[#444E74]  h-[18%] font-black text-center pt-4 mb-5 tracking-wider text-md md:text-lg lg:text-2xl ">
         LEADER BOARD
       </h1>
-      <BoardTemplate user={first}/>
-      <BoardTemplate user={second}/>
-      <BoardTemplate user={third}/>
+      <BoardTemplate number="01" user={first}/>
+      <BoardTemplate number="02" user={second}/>
+      <BoardTemplate number="03" user={third}/>
     </div>
   );
 }
