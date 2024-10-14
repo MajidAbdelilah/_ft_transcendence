46e365317d9b5d086a5710537b2b14b7e53ae815
Abdellah Azeroual <azero853@gmail.com>
2024-10-14 13:13:37 +0100

    2FA INPUT/RESPNS

--- src/app/(Home)/Settings/2fa.jsx
+++ src/app/(Home)/Settings/2fa.jsx
@@ -39,9 +39,9 @@ function TwoFA({setIs2FA}) {
       <div className="fixed inset-0 backdrop-blur-sm flex justify-center items-center animate-fadeIn  absolute top-0 left-0">
             <div className="bg-[#F4F4FF] flex flex-col items-center shadow-lg rounded-xl w-[95%] h-[80%] sm:h-[90%] border-solid border-[#BCBCC9] border-2 max-w-[900px] max-h-[900px] min-h-[580px] rounded-xl pt-8 animate-scaleIn">
               <div className="relative flex flex-col items-center w-full h-full">
-                <h1 className="text-2xl sm:text-3xl font-bold tracking-wide text-[#242F5C] pt-4 sm:pt-8 text-center">Two Factor Authenticator</h1>
+                <h1 className="text-lg sm:text-3xl font-bold tracking-wide text-[#242F5C] pt-4 sm:pt-8 text-center">Two Factor Authenticator</h1>
                 <hr className="w-[70%] h-[3px] bg-[#CDCDE5] border-none rounded-full mt-4 sm:mt-8" />
-                <h1 className="text-lg sm:text-xl font-bold tracking-wide text-[#242F5C] pt-4 sm:pt-8 text-center pt-20 pb-8">Please check your E-mail for the security code.</h1>
+                <h1 className="text-md sm:text-xl font-bold tracking-wide text-[#242F5C] pt-4 sm:pt-8 text-center pt-20 pb-8">Please check your E-mail for the security code.</h1>
                 <h1 className="text-lg sm:text-xl font-bold tracking-wide text-[#242F5C] pt-4 sm:pt-8 text-right absolute top-[38%] sm:top-[25%] left-[6%] sm:left-[17%]">2FA Security code</h1>
                 <div className="flex flex-col items-center bg-[#DAE4FF] w-[90%] sm:w-[70%] h-[10%] rounded-xl my-28 sm:my-28 relative">
                   <input 
