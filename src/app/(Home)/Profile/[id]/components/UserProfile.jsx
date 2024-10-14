609def3e3bbdc6f84ecc56e1272d2edf63d6f71c
Hassan Ed-Dyb <hed-dyb@e3r3p5.1337.ma>
2024-10-14 16:59:25 +0100

    save

---
+++ src/app/(Home)/Profile/[id]/components/UserProfile.jsx
@@ -0,0 +1,87 @@
+// import { div } from "framer-motion/client";
+// import { DashContext } from "../../Dashboard/Dashcontext";
+// import { useContext } from "react";
+
+import { BsChatLeftText } from "react-icons/bs";
+import { MdOutlinePersonAddAlt } from "react-icons/md";
+import { LuUserX } from "react-icons/lu";
+// import { DashContext } from "../../Dashboard/Dashcontext";
+// import { useContext } from "react";
+// -- colors -----------------------------------------------------co
+//  #F4F4FF   #242F5C   #8988DE   #BCBCC9   #F4F4FF   #EAEAFF   #C0C7E0
+
+
+
+
+
+function Part1({user}) {
+  return (
+    <div className="part1 relative w-1/3 p-2 rounded-l-2xl bg-[#F4F4FF] border-[#BCBCC9] border-r-2 min-w-32 ">
+
+
+    <div className="flex flex-col items-center">
+        <img
+        src={user.avatar}
+        alt="Profile"
+        className="absolute w-20 h-20 rounded-full border-2 border-[#BCBCC9] -top-10  shadow-md shadow-[#BCBCC9]"
+        />
+      <div className="mt-12 text-sm md:text-md lg:text-lg xl:text-xl font-bold text-[#242F5C]">
+        {user.name}
+      </div>
+      <span className="text-xs md:text-sm lg:text-md xl:text-lg mt-1 text-[#8988DE]">{user.status}</span>
+
+      <div className="flex flex-row mt-2 text-[#242F5C] ">
+        <BsChatLeftText className="mr-1 text-lg lg:text-xl" />
+        <MdOutlinePersonAddAlt className="ml-1 text-xl lg:text-2xl" />
+      </div>
+    </div>
+  </div>
+  );
+}
+
+function Part2({user}) {
+  return (
+      <div className="part2 w-2/3 p-4 flex flex-col items-end ml-auto   ">
+
+      <LuUserX className="blockIcon text-[#242F5C] text-3xl" />
+
+      <div className="level flex flex-col items-start w-full mb-4">
+        <span className=" text-[#242F5C] font-semibold text-xs ">Level {user.level}</span>
+
+        
+        <div className="relative w-full h-3 bg-gray-300 rounded-full mt-1 ">
+          
+          <div
+            className="absolute top-0 left-0 h-3 bg-[#8988DE] rounded-full "
+            style={{ width: `${user.level * 20}%` }} 
+          ></div>
+        </div>
+
+        
+        <div className="flex justify-between w-full mt-1 text-[#242F5C] text-xs ">
+          <span >Next level</span>
+          <span >Level {user.level + 1}</span>
+        </div>
+
+
+      </div>
+      <button className="gameStats bg-[#242F5C] p-1  px-2 text-[#F4F4FF] text-xs rounded-full font-semibold">
+          GAME STATS
+      </button>
+    </div>
+  )
+}
+
+
+export default function UserProfile({user}) {
+  
+
+  return (
+    <div className="flex shadow-md shadow-[#BCBCC9] border border-[#BCBCC9] rounded-2xl bg-[#F4F4FF] h-40 w-[80%] mt-10">
+      
+      <Part1 user={user} />
+      <Part2 user={user}/>
+
+    </div>
+  );
+}
--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -1,7 +1,11 @@
+'use client';
 import Image from "next/image";
 import { useClickAway } from "@uidotdev/usehooks";
 import { Montserrat } from "next/font/google";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
+import { motion } from "framer-motion"
+import Link from "next/link";
+// import authService from './authService';
 
 
 
@@ -10,17 +14,60 @@ const montserrat = Montserrat({
   variable: "--font-montserrat",
 });
 
+const logout = async () => {
+  try {
+    await authService.logout();
+    // Handle successful logout (e.g., clear app state, redirect)
+  } catch (error) {
+    console.error('Logout failed', error);
+  }
+};
+
+
+const LogoutProfile = () => {
+  return (
+    <div className="flex flex-row items-center m-3 justify-content relative gap-2 cursor-pointer" onClick={logout}>
+      <Image src="/images/logout.svg" alt="profile" width={50} height={50} className="w-[18px] h-[18px]" />
+      <h1 className="text-base font-medium text-[#242F5C]">Log Out</h1>
+    </div>
+  );
+}
+
+
+const ProfileSetting = () => {
+  return (
+    <Link href="/Settings">
+      <div className="flex flex-row items-center m-3 justify-content relative gap-2 cursor-pointer">
+        <Image src="/images/settings.svg" alt="profile" width={50} height={50} className="w-[18px] h-[18px]" />
+        <h1 className="text-base font-medium text-[#242F5C]">Account Settings</h1>
+      </div>
+    </Link>
+  );
+}
+
+const ProfileInfo = () => {
+  return (
+    <div className="flex flex-row items-center m-3 justify-content relative gap-2 cursor-pointer">
+      <Image src="/images/avatarAcc.svg" alt="profile" width={50} height={50} className="w-[18px] h-[18px]" />
+      <h1 className="text-base font-medium text-[#242F5C]">View Profile</h1>
+    </div>
+  );
+}
+
 function Navbar() {
+
   const [userDropdown, setUserDropdown] = useState(false);
   const [notificationDropdown, setNotificationDropdown] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
 
   const userDropdownRef = useRef(null);
   const notificationDropdownRef = useRef(null);
-  
+
   useClickAway(userDropdownRef, () => {
     setUserDropdown(false);
   });
-  
+
   useClickAway(notificationDropdownRef, () => {
     setNotificationDropdown(false);
   });
@@ -30,54 +77,51 @@ function Navbar() {
     setUserDropdown(prev => !prev);
     setNotificationDropdown(false);
   };
-  
+
   const toggleNotificationDropdown = (e) => {
     e.stopPropagation();
     setNotificationDropdown(prev => !prev);
     setUserDropdown(false);
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+
+  }, []);
+
+
   return (
     <nav
-      className={`bg-[#F4F4FF] py-4 h-[90px] flex items-center shadow-md shadow-[#BCBCC9] ${montserrat.className}`}
+      className={`bg-[#F4F4FF] py-4 h-[90px] flex items-center shadow-md shadow-[#BCBCC9] z-[9] ${montserrat.className}`}
     >
-      {/* ------------------------------------------------------------------------------------- */}
-      <div className="flex justify-end flex-auto sm:gap-5 gap-3 sm:mr-10 ml-5">
+      <div className="flex justify-end flex-auto sm:gap-5 gap-3 sm:mr-10">
         <div className="relative">
-            <input
-              type="text"
-              placeholder="Search..."
-              className="sm:py-3 shadow-sm shadow-[#BCBCC9] sm:w-[280px] py-[8px] w-[200px]  pl-[2.5rem] rounded-full bg-[#D7D7EA] text-[#242F5C] focus:outline-none focus:ring-2 focus:ring-[#3CDCDE5]"
+          <input
+            type="text"
+            placeholder="Search..."
+            className="sm:py-3 shadow-sm shadow-[#BCBCC9] sm:w-[280px] py-[8px] w-[200px]  pl-[2.5rem] rounded-full bg-[#D7D7EA] text-[#242F5C] focus:outline-none focus:ring-2 focus:ring-[#3CDCDE5]"
+          />
+          <svg
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 op h-5 w-5 text-gray-400"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
             />
-            <svg
-              className="absolute left-3 top-1/2 transform -translate-y-1/2 op h-5 w-5 text-gray-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
+          </svg>
         </div>
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         <div ref={notificationDropdownRef}>
           <Image
             onClick={toggleNotificationDropdown}
@@ -89,7 +133,7 @@ function Navbar() {
           />
         </div>
         <div ref={userDropdownRef}
-          className="flex items-center justify-center sm:w-12 sm:h-12 w-10 h-10 rounded-full bg-white text-white relative mr-2 cursor-pointer"
+          className="flex items-center justify-center sm:w-12 sm:h-12 w-10 h-10 rounded-full bg-white text-white relative mr-2"
           onClick={toggleUserDropdown}
         >
           <Image
@@ -97,7 +141,7 @@ function Navbar() {
             type="button"
             dropdown-toggle="userDropdown"
             dropdown-placement="bottom-start"
-            className="sm:w-10 sm:h-10 w-8 h-8 rounded-full cursor-pointer "
+            className="sm:w-10 sm:h-10 w-8 h-8 rounded-full bg-[#D7D7EA] cursor-pointer rounded-full"
             src="/images/avatar.svg"
             alt="User dropdown"
             width="100"
@@ -113,17 +157,36 @@ function Navbar() {
             width="50"
             height="50"
           />
-          {userDropdown &&(
-            <div
-              className="w-[200px] h-[200px] bg-[#EAEAFF] absolute bottom-[-210px] right-[3px] z-[10] rounded-[5px]"
+          {userDropdown && (
+            <motion.div
+              className="w-[220px] h-[210px] bg-[#EAEAFF] border-2 border-solid border-[#C0C7E0] absolute bottom-[-215px] right-[3px] z-[10] rounded-[5px] shadow shadow-[#BCBCC9]"
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 260,
+                damping: 30
+              }}
             >
-              <p>John Doe</p>
-            </div>
+              <h1 className="text-lg font-medium text-[#242F5C] p-4">My Account</h1>
+              <hr className="w-[100%] h-[1px] bg-[#CDCDE5] border-none rounded-full" />
+              <ProfileInfo />
+              <ProfileSetting />
+              <hr className="w-[100%] h-[1px] bg-[#CDCDE5] border-none rounded-full" />
+              <LogoutProfile />
+            </motion.div>
           )}
           {notificationDropdown && (
-            <div
-              className="w-[400px] h-[200px] bg-[#EAEAFF] absolute bottom-[-210px] right-[70px] z-[10] rounded-[5px]"
-            ></div>
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{
+                type: "spring",
+                stiffness: 260,
+                damping: 30
+              }}
+              className="w-[250px] h-[200px] sm:w-[400px] sm:h-[200px] bg-[#EAEAFF] absolute bottom-[-210px] right-[70px] z-[10] rounded-[5px] border-2 border-solid border-[#C0C7E0] shadow shadow-[#BCBCC9]"
+            ></motion.div>
           )}
         </div>
       </div>
