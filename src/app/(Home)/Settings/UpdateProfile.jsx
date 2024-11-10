e9fcb456f49af893bc61a03c4b98f6fa7077085a
Hassan Ed-Dyb <hed-dyb@e2r3p3.1337.ma>
2024-11-10 17:49:13 +0100

    save

---
+++ src/app/(Home)/Settings/UpdateProfile.jsx
@@ -0,0 +1,148 @@
+
+
+import Image from 'next/image';
+import React, { useState } from 'react';
+
+
+export default function UpdateProfile({setIsProfile}) 
+{
+    // Part 1 : handling updating image   ######################################################################
+    // Part 2 : handel updating informations   ######################################################################
+
+    const [formData, setFormData] = useState({
+        username: '',
+        currentPassword: '',
+        newPassword: '',
+        confirmPassword: ''
+    });
+
+    const handleOnChange = (e) => 
+    {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]: value});
+    }
+
+
+
+
+    // part 3 : handle the submiting the form   ######################################################################
+    const handleSubmit = (e) => {
+        const data = {
+            username: formData.username,
+            current_password: formData.currentPassword,
+            new_password: formData.newPassword,
+            // confirmPassword: formData.confirmPassword,
+          };
+
+          console .log(data);
+    }
+
+
+    return (
+    <div className="fixed inset-0 backdrop-blur-sm flex justify-center items-start sm:items-center absolute top-0 left-0 w-full h-full animate-fadeIn p-4">
+    <div className="bg-[#F4F4FF] flex flex-col items-center shadow-lg rounded-xl w-[95%] sm:w-full h-[85vh] sm:h-auto border-solid border-[#BCBCC9] border-2 max-w-[800px] sm:mt-[160px] max-h-[100vh] sm:max-h-[900px] min-h-[500px] sm:min-h-[800px] overflow-y-auto pt-4 sm:pt-8 animate-scaleIn">
+      <div className="relative flex flex-col items-center w-full h-full px-3 sm:px-0">
+      {/* closeWindowIcon --------------------------------------------------------------------------------------- */}
+        <Image
+          src="/images/close.svg"
+          alt="Close"
+          width={32}
+          height={32}
+          className="closeWindowIcon absolute top-[-8px] sm:top-[15px] right-3 sm:right-11 cursor-pointer w-[22px] h-[22px] sm:w-[32px] sm:h-[32px]"
+          onClick={() => setIsProfile(false)}
+        />
+        {/* Updating Image --------------------------------------------------------------------------------------- */}
+        <div className="flex flex-col items-center relative">
+          <Image
+            src="/images/DefaultAvatar.svg"
+            alt="Profile"
+            width={100}
+            height={100}
+            className="w-[100px] h-[100px] sm:w-[150px] sm:h-[150px] cursor-pointer rounded-full object-cover"
+          />
+          <div className="absolute bottom-[-5px] right-1 sm:right-6">
+            <Image
+              src="/images/upload.svg"
+              alt="Camera"
+              width={32}
+              height={32}
+              className="w-6 h-6 sm:w-8 sm:h-8 cursor-pointer"
+              onClick={() => document.getElementById('fileInput').click()}
+            />
+          </div>
+        </div>
+
+        <input
+          type="file"
+          id="fileInput"
+          className="hidden"
+          onChange={(e) => handleFileUpload(e)}
+        />
+
+        <h1 className="text-lg sm:text-2xl font-bold tracking-wide text-[#242F5C] pt-4 sm:pt-8">Update Profile</h1>
+        {/*  Updating Data --------------------------------------------------------------------------------------- */}
+        <form className="w-full h-full flex flex-col items-center justify-center">
+          <div className='w-[85%] sm:max-w-[350px] mt-2 sm:mt-4'>
+            <label htmlFor="username" className="block mb-1 sm:mb-2 text-sm sm:text-lg font-bold text-[#242F5C]">Username</label>
+            <input 
+              type="text" 
+              id="username" 
+              name="username" 
+              className="bg-[#F8FBFF] border text-gray-900 text-sm rounded-[10px] focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 sm:p-3.5" 
+              placeholder="Username" 
+              required 
+              pattern="^[^\s]*$" 
+              title="Username should not contain spaces"
+              value={formData.username}
+              onChange={handleOnChange}
+
+            />
+
+            <label htmlFor="Current Password" className="block mb-1 sm:mb-2 mt-3 sm:mt-5 text-sm sm:text-lg font-bold text-[#242F5C]">Current Password *</label>
+            <input 
+              type="password" 
+              id="Current Password" 
+              className="bg-[#F8FBFF] border text-gray-900 text-sm rounded-[10px] focus:ring-blue-500 mb-3 sm:mb-5 focus:border-blue-500 block w-full p-2.5 sm:p-3.5" 
+              placeholder="Current Password" 
+              required 
+              value={formData.currentPassword} 
+              onChange={handleOnChange}
+            />
+
+            <label htmlFor="New Password" className="block mb-1 sm:mb-2 mt-3 sm:mt-5 text-sm sm:text-lg font-bold text-[#242F5C]">New Password</label>
+            <input 
+              type="password" 
+              id="New Password" 
+              className="bg-[#F8FBFF] border text-gray-900 text-sm rounded-[10px] focus:ring-blue-500 mb-3 sm:mb-5 focus:border-blue-500 block w-full p-2.5 sm:p-3.5" 
+              placeholder="Enter your password" 
+              required 
+              value={formData.newPassword}
+              onChange={handleOnChange}
+            />
+
+            <label htmlFor="Confirm Password" className="block mb-1 sm:mb-2 mt-3 sm:mt-5 text-sm sm:text-lg font-bold text-[#242F5C]">Confirm Password</label>
+            <input 
+              type="password" 
+              id="Confirm Password" 
+              className="bg-[#F8FBFF] border text-gray-900 text-sm rounded-[10px] focus:ring-blue-500 mb-3 sm:mb-5 focus:border-blue-500 block w-full p-2.5 sm:p-3.5" 
+              placeholder="Confirm your password" 
+              required 
+              value={formData.confirmPassword}
+              onChange={handleOnChange}
+            />
+
+            <button 
+              type="submit" 
+              className="text-white bg-[#111B47] focus:ring-4 focus:outline-none font-semibold rounded-[10px] text-sm sm:text-lg w-full px-4 sm:px-20 py-2.5 sm:py-3 text-center mb-4 sm:mb-5 transition-transform duration-300 ease-in-out transform hover:scale-105"
+              onClick={(e) => handleSubmit(e)}
+            >
+              Update
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>);
+}
+
+
--- src/app/(Home)/Settings/page.jsx
+++ src/app/(Home)/Settings/page.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { motion } from "framer-motion"
 import TwoFA from "./2fa";
 import withAuth from "../../HOC.tsx";
-
+import UpdateProfile from "./UpdateProfile"
 
 
 const montserrat = Montserrat({
@@ -30,9 +30,7 @@ function Settings() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  const handleFileUpload = (e) => {
-    console.log(e.target.files[0]);
-  };
+
 
   return (
 
@@ -65,7 +63,7 @@ function Settings() {
 
 
 
-          {/* My chnages --------------------------------------------------------------------------- */}
+          
           <div onClick={() => { setIsProfile(!isProfile) }} className="w-full max-w-md rounded-xl flex items-center justify-center bg-[#D7D7EA] shadow-md shadow-[#BCBCC9] p-4 cursor-pointer hover:bg-[#E1E1EF] transition-colors duration-300 transition-transform duration-300 transform hover:scale-105 ease-in-out">
             <Image src="/images/profile.svg" alt="Profile" width={64} height={64} className="w-12 h-12 sm:w-16 sm:h-16" />
             {!isIcon && (
@@ -83,104 +81,12 @@ function Settings() {
 
 
 
-          
+
         </div>
       </motion.div>
-      {isProfile && (
-        <div className="fixed inset-0 backdrop-blur-sm flex justify-center items-start sm:items-center absolute top-0 left-0 w-full h-full animate-fadeIn p-4">
-        <div className="bg-[#F4F4FF] flex flex-col items-center shadow-lg rounded-xl w-[95%] sm:w-full h-[85vh] sm:h-auto border-solid border-[#BCBCC9] border-2 max-w-[800px] sm:mt-[160px] max-h-[100vh] sm:max-h-[900px] min-h-[500px] sm:min-h-[800px] overflow-y-auto pt-4 sm:pt-8 animate-scaleIn">
-          <div className="relative flex flex-col items-center w-full h-full px-3 sm:px-0">
-            <Image
-              src="/images/close.svg"
-              alt="Close"
-              width={32}
-              height={32}
-              className="absolute top-[-8px] sm:top-[15px] right-3 sm:right-11 cursor-pointer w-[22px] h-[22px] sm:w-[32px] sm:h-[32px]"
-              onClick={() => setIsProfile(false)}
-            />
-            
-            <div className="flex flex-col items-center relative">
-              <Image
-                src="/images/DefaultAvatar.svg"
-                alt="Profile"
-                width={100}
-                height={100}
-                className="w-[100px] h-[100px] sm:w-[150px] sm:h-[150px] cursor-pointer rounded-full object-cover"
-              />
-              <div className="absolute bottom-[-5px] right-1 sm:right-6">
-                <Image
-                  src="/images/upload.svg"
-                  alt="Camera"
-                  width={32}
-                  height={32}
-                  className="w-6 h-6 sm:w-8 sm:h-8 cursor-pointer"
-                  onClick={() => document.getElementById('fileInput').click()}
-                />
-              </div>
-            </div>
-  
-            <input
-              type="file"
-              id="fileInput"
-              className="hidden"
-              onChange={(e) => handleFileUpload(e)}
-            />
-  
-            <h1 className="text-lg sm:text-2xl font-bold tracking-wide text-[#242F5C] pt-4 sm:pt-8">Update Profile</h1>
-  
-            <form className="w-full h-full flex flex-col items-center justify-center">
-              <div className='w-[85%] sm:max-w-[350px] mt-2 sm:mt-4'>
-                <label htmlFor="username" className="block mb-1 sm:mb-2 text-sm sm:text-lg font-bold text-[#242F5C]">Username</label>
-                <input 
-                  type="email" 
-                  id="email" 
-                  className="bg-[#F8FBFF] border text-gray-900 text-sm rounded-[10px] focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 sm:p-3.5" 
-                  placeholder="Username" 
-                  required 
-                />
-  
-                <label htmlFor="Current Password" className="block mb-1 sm:mb-2 mt-3 sm:mt-5 text-sm sm:text-lg font-bold text-[#242F5C]">Current Password *</label>
-                <input 
-                  type="password" 
-                  id="Current Password" 
-                  className="bg-[#F8FBFF] border text-gray-900 text-sm rounded-[10px] focus:ring-blue-500 mb-3 sm:mb-5 focus:border-blue-500 block w-full p-2.5 sm:p-3.5" 
-                  placeholder="Current Password" 
-                  required 
-                />
-  
-                <label htmlFor="New Password" className="block mb-1 sm:mb-2 mt-3 sm:mt-5 text-sm sm:text-lg font-bold text-[#242F5C]">New Password</label>
-                <input 
-                  type="password" 
-                  id="New Password" 
-                  className="bg-[#F8FBFF] border text-gray-900 text-sm rounded-[10px] focus:ring-blue-500 mb-3 sm:mb-5 focus:border-blue-500 block w-full p-2.5 sm:p-3.5" 
-                  placeholder="Enter your password" 
-                  required 
-                />
-  
-                <label htmlFor="Confirm Password" className="block mb-1 sm:mb-2 mt-3 sm:mt-5 text-sm sm:text-lg font-bold text-[#242F5C]">Confirm Password</label>
-                <input 
-                  type="password" 
-                  id="Confirm Password" 
-                  className="bg-[#F8FBFF] border text-gray-900 text-sm rounded-[10px] focus:ring-blue-500 mb-3 sm:mb-5 focus:border-blue-500 block w-full p-2.5 sm:p-3.5" 
-                  placeholder="Confirm your password" 
-                  required 
-                />
-  
-                <button 
-                  type="submit" 
-                  className="text-white bg-[#111B47] focus:ring-4 focus:outline-none font-semibold rounded-[10px] text-sm sm:text-lg w-full px-4 sm:px-20 py-2.5 sm:py-3 text-center mb-4 sm:mb-5 transition-transform duration-300 ease-in-out transform hover:scale-105"
-                >
-                  Update
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-      )}
-      {is2FA && (
-        <TwoFA setIs2FA={setIs2FA}/>
-      )}
+      {/* My chnages --------------------------------------------------------------------------- */}
+      {isProfile && (<UpdateProfile setIsProfile={setIsProfile} />)} 
+      {is2FA && (    <TwoFA         setIs2FA={setIs2FA}       /> )}
     </div>
   );
 }
