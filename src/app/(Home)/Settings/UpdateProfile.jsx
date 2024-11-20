a2acdc62a6a7cca9fc2b89ed476fdc7fdc940619
Hassan Ed-Dyb <hed-dyb@e2r4p3.1337.ma>
2024-11-20 18:39:16 +0100

    save

--- src/app/(Home)/Settings/UpdateProfile.jsx
+++ src/app/(Home)/Settings/UpdateProfile.jsx
@@ -90,6 +90,7 @@ export default function UpdateProfile({setIsProfile})
           const successMsg = result.data.message;
           console.log(successMsg);
           toast.success(successMsg);
+          window.location.reload();
         }
       }
       catch (error) {
