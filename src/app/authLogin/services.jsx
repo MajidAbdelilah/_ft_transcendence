6acc6b3a55752f9ba4bb811dbd8df9830ed84234
Hassan Ed-Dyb <hed-dyb@e2r4p3.1337.ma>
2024-11-21 12:07:31 +0100

    save

--- src/app/authLogin/services.jsx
+++ src/app/authLogin/services.jsx
@@ -5,34 +5,30 @@ import toast, { Toaster } from 'react-hot-toast';
 
 async function handleVerification(code, router)  {
     
-    // const router = useRouter();
-    // console.log("code : ", code);
+    const router = useRouter();
+    console.log("code : ", code);
 
+    try {
 
-    router.push('/Dashboard');
-
-
-
-    // try {
-
-    //     const result = await axios.post('http://127.0.0.1:8000/api/handleVerification',
-    //     {code : code},
-    //     { withCredentials: true, headers: {} });
+        const result = await axios.post('http://127.0.0.1:8000/api/handleVerification',
+        {code : code},
+        { withCredentials: true, headers: {} });
 
 
 
 
-    //     if(result.data.message === '2fa is done') {
-    //         toast.success('Verification done successfully!');
-    //         router.push('/Dashboard');
-    //     } else {
-    //         toast.error(result.data.message);
-    //     }
+        if(result.data.message === '2fa is done') // check what the back end send later ...
+        {
+            toast.success('Verification done successfully!');
+            router.push('/Dashboard');
+        } else {
+            toast.error(result.data.message);
+        }
 
-    // } catch (error) {
-    //     console.log('Post request failed .......');
-    //     console.log(result);
-    // }
+    } catch (error) {
+        console.log('Post request failed .......');
+        console.log(result);
+    }
 
 
   }
