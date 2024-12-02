0dca14b5e18cb227a5ee7c60605cd377701a3250
amajid <abdelilahmajid012@gmail.com>
2024-12-02 18:50:14 +0100

    init

--- back/django/turn/consumers.py
+++ back/django/turn/consumers.py
@@ -6,8 +6,8 @@ class PingPongConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = f'pingpong_{self.room_name}'
-        self.width = 100
-        self.height = 100
+        self.width = 1000
+        self.height = 600
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -16,10 +16,10 @@ class PingPongConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
         self.players = {
-            'player1': {'y': 50, 'h', 'x' : 10, 'width' : 10, 'direction': None},
-            'player2': {'y': 50, 'direction': None}
+            'player1': {'y': self.height/2 - 25, 'height': 100, 'x' : 0, 'width' : 10, 'direction': None, 'score': 0},
+            'player2': {'y': self.height/2 - 25, 'height': 100, 'x' : self.width - 10, 'width' : 10, 'direction': None, 'score': 0}
         }
-        self.ball = {'x': 50, 'y': 50, 'vx': 1, 'vy': 1}
+        self.ball = {'x': self.width / 2, 'y': self.height / 2, 'radius': 5, 'vx': 5, 'vy': 5}
 
         asyncio.create_task(self.game_loop())
 
@@ -57,21 +57,34 @@ class PingPongConsumer(AsyncWebsocketConsumer):
         self.ball['y'] += self.ball['vy']
 
         # Ball collision with top and bottom walls
-        if self.ball['y'] <= 0 or self.ball['y'] >= self.height:
+        if self.ball['y'] - self.ball['radius'] <= 0 or self.ball['y'] + self.ball['radius'] >= self.height:
             self.ball['vy'] *= -1
 
         # Update player positions
         for player in self.players:
             if self.players[player]['direction'] == 'up' and self.players[player]['y'] > 0:
-                self.players[player]['y'] -= 1
-            elif self.players[player]['direction'] == 'down' and self.players[player]['y'] < self.height:
-                self.players[player]['y'] += 1
+                self.players[player]['y'] -= 10
+            elif self.players[player]['direction'] == 'down' and self.players[player]['y'] < self.height - self.players[player]['height']:
+                self.players[player]['y'] += 10
 
         # Ball collision with player paddles
-        if self.ball['x'] <= 0 and self.players['player1']['y'] - 10 <= self.ball['y'] <= self.players['player1']['y'] + 10:
+        if (self.ball['x'] - self.ball['radius'] <= self.players['player1']['x'] + self.players['player1']['width'] and
+            self.players['player1']['y'] <= self.ball['y'] <= self.players['player1']['y'] + self.players['player1']['height']):
             self.ball['vx'] *= -1
-        elif self.ball['x'] >= self.width and self.players['player2']['y'] - 10 <= self.ball['y'] <= self.players['player2']['y'] + 10:
+        elif (self.ball['x'] + self.ball['radius'] >= self.players['player2']['x'] and
+              self.players['player2']['y'] <= self.ball['y'] <= self.players['player2']['y'] + self.players['player2']['height']):
             self.ball['vx'] *= -1
+
+        # Check for goals
+        if self.ball['x'] - self.ball['radius'] <= 0:
+            self.players['player2']['score'] += 1
+            self.reset_ball()
+        elif self.ball['x'] + self.ball['radius'] >= self.width:
+            self.players['player1']['score'] += 1
+            self.reset_ball()
+
+    def reset_ball(self):
+        self.ball = {'x': self.width / 2, 'y': self.height / 2, 'radius': 5, 'vx': self.ball['vx'] * -1, 'vy': 5}
 
     async def game_update(self, event):
         await self.send(text_data=json.dumps({
