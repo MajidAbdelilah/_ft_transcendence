7c05fa500f80f47d85b75335fbbf719834e94a4d
amajid <abdelilahmajid012@gmail.com>
2024-12-04 17:50:06 +0100

    init

--- back/django/turn/consumers.py
+++ back/django/turn/consumers.py
@@ -34,11 +34,18 @@ class PingPongConsumer(AsyncWebsocketConsumer):
         if self.room_name not in self.room_var:
             self.room_var[self.room_name] = {
                 'players': {
-                    'player1': {'y': self.height/2 - 25, 'height': 100, 'x': 0, 'width': 10, 'direction': None, 'score': 0, 'full': False, 'username': '', 'game_start': False},
-                    'player2': {'y': self.height/2 - 25, 'height': 100, 'x': self.width - 10, 'width': 10, 'direction': None, 'score': 0, 'full': False, 'username': '', 'game_start': False}
+                    'player1': {'y': self.height/2 - 25, 'height': 100, 'x': 0, 'width': 10, 'direction': None, 'score': 0, 'full': False, 'username': '', 'game_start': False, 'current_match': 'match1'},
+                    'player2': {'y': self.height/2 - 25, 'height': 100, 'x': self.width - 10, 'width': 10, 'direction': None, 'score': 0, 'full': False, 'username': '', 'game_start': False, 'current_match': 'match1'},
+                    'player3': {'y': self.height/2 - 25, 'height': 100, 'x': 0, 'width': 10, 'direction': None, 'score': 0, 'full': False, 'username': '', 'game_start': False, 'current_match': 'match2'},
+                    'player4': {'y': self.height/2 - 25, 'height': 100, 'x': self.width - 10, 'width': 10, 'direction': None, 'score': 0, 'full': False, 'username': '', 'game_start': False, 'current_match': 'match2'}
                 },
                 'ball': {'x': self.width / 2, 'y': self.height / 2, 'radius': 5, 'vx': 5, 'vy': 5},
-                'game_start': True
+                'game_start': True,
+                'matches': {
+                    'match1': {'player1': 'player1', 'player2': 'player2', 'winner': None},
+                    'match2': {'player1': 'player3', 'player2': 'player4', 'winner': None},
+                    'final': {'player1': None, 'player2': None, 'winner': None}
+                }
             }            
             asyncio.create_task(self.game_loop())
 
@@ -94,14 +101,14 @@ class PingPongConsumer(AsyncWebsocketConsumer):
                     'start_game': self.room_var[self.room_name]['game_start']
                 }
             )
-            if(not self.room_var[self.room_name]['players']['player1']['game_start'] or not self.room_var[self.room_name]['players']['player2']['game_start']):
-                await asyncio.sleep(1/60)
-                continue
-            self.update_game_state()
             if not self.room_var[self.room_name]['game_start']:
                 self.disconnect(1000)
                 del self.room_var[self.room_name]
                 break
+            if(not self.room_var[self.room_name]['players']['player1']['game_start'] or not self.room_var[self.room_name]['players']['player2']['game_start']):
+                await asyncio.sleep(1/60)
+                continue
+            await self.update_game_state()
             await asyncio.sleep(1/60)
 
     async def game_update(self, event):
@@ -115,16 +122,13 @@ class PingPongConsumer(AsyncWebsocketConsumer):
             'start_game': self.room_var[self.room_name]['game_start']
         }))
 
-    def update_game_state(self):
+    async def update_game_state(self):
         if self.room_name not in self.room_var:
             return
         ball = self.room_var[self.room_name]['ball']
         players = self.room_var[self.room_name]['players']
         ball['x'] += ball['vx']
         ball['y'] += ball['vy']
-        # Ball collision with top and bottom walls
-        if ball['y'] - ball['radius'] <= 0 or ball['y'] + ball['radius'] >= self.height:
-            ball['vy'] *= -1
         # Update player positions
         for player in players:
             if players[player]['direction'] == 'up' and players[player]['y'] > 0:
@@ -140,18 +144,80 @@ class PingPongConsumer(AsyncWebsocketConsumer):
             ball['vx'] *= -1
         # Check for goals
         if ball['x'] - ball['radius'] <= 0:
-            players['player2']['score'] += 1
+            await self.update_score('player2')
             self.reset_ball()
         elif ball['x'] + ball['radius'] >= self.width:
-            players['player1']['score'] += 1
+            await self.update_score('player1')
             self.reset_ball()
-        # Check for winning condition
-        if players['player1']['score'] >= 5:
-            asyncio.create_task(self.save_game_data('player1'))
-        elif players['player2']['score'] >= 5:
-            asyncio.create_task(self.save_game_data('player2'))
+        # Ball collision with top and bottom walls
+        if ball['y'] - ball['radius'] <= 0 or ball['y'] + ball['radius'] >= self.height:
+            ball['vy'] *= -1
+        # # Check for winning condition
+        # if players['player1']['score'] >= 5:
+        #     asyncio.create_task(self.save_game_data('player1'))
+        # elif players['player2']['score'] >= 5:
+        #     asyncio.create_task(self.save_game_data('player2'))
             
+    async def update_score(self, player):
+        print(player)
+        self.room_var[self.room_name]['players'][player]['score'] += 1
+        print(self.room_var[self.room_name]['players'][player]['score'])
+        if self.room_var[self.room_name]['players'][player]['score'] >= 10:  # Assuming 10 points to win
+            await self.end_match(player)
 
+    async def end_match(self, winner):
+        current_match = self.room_var[self.room_name]['players'][winner]['current_match']
+        self.room_var[self.room_name]['matches'][current_match]['winner'] = winner
+
+        player1_score = self.room_var[self.room_name]['players']['player1']['score']
+        player2_score = self.room_var[self.room_name]['players']['player2']['score']
+        player1_username = self.room_var[self.room_name]['players']['player1']['username']
+        player2_username = self.room_var[self.room_name]['players']['player2']['username']
+        
+        # Save the game data to the database
+        match = Match(
+            player1_username=player1_username,
+            player2_username=player2_username,
+            player1_score=player1_score,
+            player2_score=player2_score,
+            winner=winner,
+            date=timezone.now()
+        )
+        await database_sync_to_async(match.save)()
+
+        # Reset the game state
+        self.room_var[self.room_name]['players']['player1']['score'] = 0
+        self.room_var[self.room_name]['players']['player2']['score'] = 0
+        self.reset_ball()
+        self.room_var[self.room_name]['game_start'] = False
+
+        if self.room_var[self.room_name]['matches']['match1']['winner'] and self.room_var[self.room_name]['matches']['match2']['winner']:
+            self.room_var[self.room_name]['matches']['final']['player1'] = self.room_var[self.room_name]['matches']['match1']['winner']
+            self.room_var[self.room_name]['matches']['final']['player2'] = self.room_var[self.room_name]['matches']['match2']['winner']
+            await self.start_final_match()
+        else:
+            self.room_var[self.room_name]['game_start'] = False  # End the tournament if final match is not ready
+
+        # Save the tournament data if final match is completed
+        if self.room_var[self.room_name]['matches']['final']['winner']:
+            tournament = Tournament(
+                player1_username=self.room_var[self.room_name]['matches']['final']['player1'],
+                player2_username=self.room_var[self.room_name]['matches']['final']['player2'],
+                winner=self.room_var[self.room_name]['matches']['final']['winner'],
+                date=timezone.now(),
+                matches=self.room_var[self.room_name]['matches']  # Include matches data
+            )
+            await database_sync_to_async(tournament.save)()
+
+    async def start_final_match(self):
+        self.room_var[self.room_name]['players']['player1']['score'] = 0
+        self.room_var[self.room_name]['players']['player2']['score'] = 0
+        self.room_var[self.room_name]['players']['player3']['score'] = 0
+        self.room_var[self.room_name]['players']['player4']['score'] = 0
+        self.room_var[self.room_name]['players'][self.room_var[self.room_name]['matches']['final']['player1']]['current_match'] = 'final'
+        self.room_var[self.room_name]['players'][self.room_var[self.room_name]['matches']['final']['player2']]['current_match'] = 'final'
+        self.room_var[self.room_name]['game_start'] = True
+        asyncio.create_task(self.game_loop())
 
     def reset_ball(self):
         ball = self.room_var[self.room_name]['ball']
@@ -164,8 +230,8 @@ class PingPongConsumer(AsyncWebsocketConsumer):
         players = self.room_var[self.room_name]['players']
         player1_score = players['player1']['score']
         player2_score = players['player2']['score']
-        player1_username = players['player1']['username'] # Replace with actual player 1 username
-        player2_username = players['player2']['username']  # Replace with actual player 2 username
+        player1_username = players['player1']['username']
+        player2_username = players['player2']['username']
         # Save the game data to the database
         match = Match(
             player1_username=player1_username,
@@ -176,10 +242,20 @@ class PingPongConsumer(AsyncWebsocketConsumer):
             date=timezone.now()
         )
         match.save()
+        # Save the tournament data if final match
+        if self.room_var[self.room_name]['matches']['final']['winner']:
+            tournament = Tournament(
+                player1_username=self.room_var[self.room_name]['matches']['final']['player1'],
+                player2_username=self.room_var[self.room_name]['matches']['final']['player2'],
+                winner=self.room_var[self.room_name]['matches']['final']['winner'],
+                date=timezone.now(),
+                matches=self.room_var[self.room_name]['matches']  # Include matches data
+            )
+            tournament.save()
         # Reset the game state
         players['player1']['score'] = 0
         players['player2']['score'] = 0
         self.reset_ball()
         self.room_var[self.room_name]['game_start'] = False
-        # disconnect the players
+            # disconnect the players
         
