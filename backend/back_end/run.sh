a55511feed545b23d06fe931a3493bfe71703728
Ghizlane Ghaya <gghaya@e2r5p14.1337.ma>
2024-12-06 23:59:21 +0100

    ok

--- backend/back_end/run.sh
+++ backend/back_end/run.sh
@@ -4,5 +4,5 @@ python3 -m venv ../myenv
 touch ../.env
 bash -c "source ../myenv/bin/activate "
 pip install -r /app/backend/requirements.txt
-# python /app/backend/manage.py runserver
+python /app/backend/manage.py runserver 0.0.0.0:8000
 # tail -f /dev/null
