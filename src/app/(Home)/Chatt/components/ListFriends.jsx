6c460fe5880173f16ee2dbdebbbe69c0c7893704
Hassan Ed-Dyb <hed-dyb@e1r6p2.1337.ma>
2024-12-09 14:38:04 +0100

    save

--- src/app/(Home)/Chatt/components/ListFriends.jsx
+++ src/app/(Home)/Chatt/components/ListFriends.jsx
@@ -67,7 +67,7 @@ export default function ListFriends({ getSelectedFriend, switchChatState }) {
                     <img
                     id="avatarButton"
                     className="rounded-full left-0 top-0 w-[45px] h-[45px]"
-                    src={friend ? friend.image_field  : "/images/Default_profile.png"}
+                    src={friend.image_field ? friend.image_field  : "/images/Default_profile.png"}
                     alt="User dropdown"
                     width={45}
                     height={45}
