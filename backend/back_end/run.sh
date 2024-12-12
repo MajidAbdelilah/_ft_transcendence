cd8f3e70225a491a6cbf58d2cb6f6cc69251eb24
Ghizlane Ghaya <gghaya@e2r10p11.1337.ma>
2024-12-12 18:56:00 +0100

    add nginx

--- backend/back_end/run.sh
+++ backend/back_end/run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+cp /app/nginx.crt /etc/ssl/certs/nginx.crt
+cp /app/nginx.key /etc/ssl/private/nginx.key
+echo "Pong_Game_1337_test" >> /etc/hostname
 python3 -m venv ../myenv
 # docker-compose up -d
 touch ../.env
 bash -c "source ../myenv/bin/activate "
 pip install -r /app/backend/requirements.txt
+systemctl start nginx
 python /app/backend/manage.py runserver 0.0.0.0:8000
+EXPOSE 80 443
 # tail -f /dev/null
