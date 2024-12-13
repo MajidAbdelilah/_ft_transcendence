1e07d11ddac0e6c3e999c8c8557c50544804c4ed
Hassan Ed-Dyb <hed-dyb@e1r7p12.1337.ma>
2024-12-13 18:05:43 +0100

    updating to 2 f a backend

--- back_end/authapp/serializers.py
+++ back_end/authapp/serializers.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta :
         model = User
-        fields=['id','username','password', 'email', 'is_2fa', '_2fa_code', 'fullname', 'image_name', 'image_field']
+        fields=['id','username','password', 'email', 'is_2fa', '_2fa_code', 'fullname', 'image_field', 'redirect_to']
         extra_kwargs = {
             'password':{'write_only':True} 
         }
